dda35659d20e17d3a011ceaecb4c13a0bf07124e
silvakhawan23 <140752643+silvakhawan23@users.noreply.github.com>
2024-03-19 10:13:07 -0300

    Update settings.py

--- projeto_integrador/projeto_integrador/settings.py
+++ projeto_integrador/projeto_integrador/settings.py
@@ -14,9 +14,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$3%k3dx(m7&==n6pc6mchq&h#+*t0v0o=-wpwz^g82q_#zpzm0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
